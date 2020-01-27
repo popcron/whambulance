2c80cafc2e02b7394ac5ef4d91de0c13cc189cc5
Phill <philldamaskin@gmail.com>
2020-01-27 16:59:39 -0500

    Add player class to send movement input

---
+++ Whambulance/Assets/Code/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// The movement component attached to this player.
+    /// </summary>
+    public PlayerMovement Movement { get; private set; }
+
+    private void Awake()
+    {
+        Movement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        //send inputs to the movement thingy
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Movement.Input = input;
+    }
+}
