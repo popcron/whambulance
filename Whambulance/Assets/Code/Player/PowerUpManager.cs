61741c144e54889cd6d5164608319496dbccf9e6
JessDesktop <45477202+RissanaZ@users.noreply.github.com>
2020-02-01 16:08:36 -0500

    Created PowerUpManager Script

---
+++ Whambulance/Assets/Code/Player/PowerUpManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //yeet me into the void
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
