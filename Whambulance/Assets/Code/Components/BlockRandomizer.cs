7db64d286f7e626be87b057d28d14fe196a9b9ce
Phill <philldamaskin@gmail.com>
2020-02-01 00:40:46 -0500

    Add block randomizer that can assign random colors

---
+++ Whambulance/Assets/Code/Components/BlockRandomizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BlockRandomizer : MonoBehaviour
+{
+    [SerializeField]
+    private Color[] colors = { };
+
+    private SpriteRenderer[] spriteRenderers = { };
+
+    private void Awake()
+    {
+        //assign each sprite renderer a random color from the array
+        foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+        {
+            //check if it exists just in case
+            if (spriteRenderer)
+            {
+                spriteRenderer.color = colors[Random.Range(0, colors.Length)];
+            }
+        }
+    }
+}
