439abdc0702f4768cfe4a1dfc20ac58a02b76665
Jasmine <44886630+Argonaviso@users.noreply.github.com>
2020-02-01 16:35:26 -0500

    Change yeet variable

--- Whambulance/Assets/Code/Player/PowerUpManager.cs
+++ Whambulance/Assets/Code/Player/PowerUpManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PowerUpManager : MonoBehaviour
 {
 
-    public int yeet;
+    public int yeet = 1;
     // Start is called before the first frame update
     void Start()
     {
