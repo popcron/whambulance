85d90f9767540d0656d92a8e278140ef705be2a3
Justin McKague <55333762+JustinMcKague@users.noreply.github.com>
2020-02-01 19:09:27 -0500

    Script organization and comments
    Moved CarHit.cs to its own folder and added comments to the script

--- Whambulance/Assets/Code/Vehicle/CarHit.cs
+++ Whambulance/Assets/Code/Vehicle/CarHit.cs
@@ -20,20 +20,24 @@ public class CarHit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Gets the car's rigidbody
         carBody = GetComponent<Rigidbody2D>();
     }
 
 
     public void LaunchCar()
     {
+        //finds the direction from the center of the player versus the center of the vehicle hit
         Vector2 launchDirection = -(player.transform.position - transform.position).normalized;
 
+        //adds an increase in velocity based on the direction
         carBody.velocity += (launchDirection * launchSpeed);
         StartCoroutine(SlowDownImpact(launchDirection));
     }
 
     IEnumerator SlowDownImpact(Vector2 direction)
     {
+        //during the time specified by carMoveFrames, the car will slow to a stop
         while (carMoveFrames > 0)
         {
             carBody.velocity -= Vector2.one * direction * Time.deltaTime;
