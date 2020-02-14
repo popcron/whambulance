59c7500c2a6345d6387bf5c5258d0599ae9d5d18
Phill <philldamaskin@gmail.com>
2020-02-14 15:00:18 -0500

    Add spawn and objective components for levels

---
+++ Whambulance/Assets/Code/Level/Objective.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Objective : Prop
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+    }
+}
---
+++ Whambulance/Assets/Code/Level/Spawnpoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Spawnpoint : Prop
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+    }
+}
