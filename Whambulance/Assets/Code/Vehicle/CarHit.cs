26f4c7c78722cc14ca7afef87d16c33063520a69
Justin McKague <55333762+JustinMcKague@users.noreply.github.com>
2020-02-14 10:49:01 -0500

    First Pass Punch Mechanic
    Added most of the functionality for the punching. (Propulsion away from player, distance from target modifier, deceleration modifer)

--- Whambulance/Assets/Code/Vehicle/CarHit.cs
+++ Whambulance/Assets/Code/Vehicle/CarHit.cs
@@ -10,6 +10,7 @@ public class CarHit : MonoBehaviour
     //Public Variables
     public float launchSpeed;
     public float spriteGrowth;
+    public float decelModifier;
 
 
     //Local Variables
@@ -17,23 +18,32 @@ public class CarHit : MonoBehaviour
     Rigidbody2D carBody;
     int iterations;
     int carMoveFrames = 90;
+    float originalDecel;
 
     // Start is called before the first frame update
     void Start()
     {
         //Gets the car's rigidbody
         carBody = GetComponent<Rigidbody2D>();
+        originalDecel = decelModifier;
     }
 
 
     public void LaunchCar()
     {
+        decelModifier = originalDecel;
         carMoveFrames = 90;
         //finds the direction from the center of the player versus the center of the vehicle hit
         Vector2 launchDirection = -(player.transform.position - transform.position).normalized;
+        Vector2 distanceFromCar = player.transform.position - transform.position;
+
+        if (Mathf.Abs(distanceFromCar.y) > 1.5f || Mathf.Abs(distanceFromCar.x) > 1.5f)
+        {
+            decelModifier -= 0.5f;
+        }
 
         //adds an increase in velocity based on the direction
-        carBody.velocity += (launchDirection * launchSpeed);
+        carBody.velocity += (launchDirection * new Vector2(launchSpeed - Mathf.Abs(distanceFromCar.x), launchSpeed - Mathf.Abs(distanceFromCar.y)));
         StartCoroutine(SlowDownImpact(launchDirection));
     }
 
@@ -42,7 +52,7 @@ public class CarHit : MonoBehaviour
         //during the time specified by carMoveFrames, the car will slow to a stop
         while (carMoveFrames > 0)
         {
-            carBody.velocity -= Vector2.one * direction * Time.deltaTime;
+            carBody.velocity -= (Vector2.one * decelModifier) * direction * Time.deltaTime;
             carMoveFrames--;
             if (carMoveFrames <= 0 || didCollide)
             {
