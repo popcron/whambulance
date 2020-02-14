2394d80c65c0c189e0ba0c06e500d3379c3f9f61
Phill <philldamaskin@gmail.com>
2020-02-13 21:47:37 -0500

    Add base HUD system

---
+++ Whambulance/Assets/Code/HUD/Elements/InGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGame : HUDElement
+{
+    /// <summary>
+    /// Only displays if the scene is set to 0
+    /// </summary>
+    public override bool ShouldDisplay
+    {
+        get
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            return scene.buildIndex == 1;
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Whambulance/Assets/Code/HUD/HUDManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class HUDManager : MonoBehaviour
+{
+    private Canvas canvas;
+    private HUDElement[] elements = { };
+
+    private void Awake()
+    {
+        canvas = GetComponent<Canvas>();
+        elements = canvas.GetComponentsInChildren<HUDElement>();
+    }
+
+    private void Update()
+    {
+        foreach (HUDElement element in elements)
+        {
+            element.gameObject.SetActive(element.ShouldDisplay);
+        }
+    }
+}
