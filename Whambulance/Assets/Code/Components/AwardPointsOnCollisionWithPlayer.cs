93d9073a56929da65b9a4c55de3cadcc29472285
Phill <philldamaskin@gmail.com>
2020-03-04 10:20:53 -0300

    Add a component that awards when collided with

---
+++ Whambulance/Assets/Code/Components/AwardPointsOnCollisionWithPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AwardPointsOnCollisionWithPlayer : Prop
+{
+    [SerializeField]
+    private string offenceName = "Nuisance";
+
+    [SerializeField]
+    private int value = 100;
+
+    private bool disturbed = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //already disturbed, dont bother checking
+        if (disturbed)
+        {
+            return;
+        }
+
+        Player player = collision.collider.GetComponentInParent<Player>();
+        if (player)
+        {
+            //collided with the player, therefor, got disturbed, therefor, give points
+            disturbed = true;
+            ScoreManager.AwardPoints(offenceName, value);
+        }
+    }
+}
