dc2632a9392a9919f383214f95a05e86ec3c6aaf
Phill <philldamaskin@gmail.com>
2020-03-04 10:04:01 -0300

    Add the patient carrying logic

--- Whambulance/Assets/Code/Level/Objective.cs
+++ Whambulance/Assets/Code/Level/Objective.cs
@@ -8,19 +8,36 @@ public class Objective : Prop
     [SerializeField]
     private float radius = 0.5f;
 
+    /// <summary>
+    /// The player that this player is being carried by.
+    /// </summary>
+    public Player CarryingPlayer { get; private set; }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, radius);
     }
 
+    public void BeganCarrying(Player player)
+    {
+        CarryingPlayer = player;
+    }
+
+    public void Dropped(Player player)
+    {
+        CarryingPlayer = null;
+    }
+
     private void Update()
     {
-        if (!GameManager.IsConcluded)
+        //only check if not being carried atm
+        if (!CarryingPlayer)
         {
             if (IsPlayerInside(radius))
             {
-                GameManager.Win();
+                //put this object onto the player
+                Player.Instance.Carry(this);
             }
         }
     }
