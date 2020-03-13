42b0855b7ee4b8e3be3f103a88cd859746b01616
Phill <philldamaskin@gmail.com>
2020-03-13 18:03:57 -0400

    Add vehicle and traffic manager

---
+++ Whambulance/Assets/Code/Managers/TrafficManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TrafficManager : MonoBehaviour
+{
+    private float nextSpawn;
+
+    private void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + 2f;
+        }
+    }
+
+    /// <summary>
+    /// Removes all vehicles from the level.
+    /// </summary>
+    public static void Clear()
+    {
+
+    }
+}
