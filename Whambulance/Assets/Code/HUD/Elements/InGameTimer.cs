617addea794b3674e81f3f98c18ab6b54ef6cfd1
Justin McKague <55333762+JustinMcKague@users.noreply.github.com>
2020-03-26 19:46:56 -0400

    Timer UI Implemented
    Timer shows in top right of screen and decreases radially.

--- Whambulance/Assets/Code/HUD/Elements/InGameTimer.cs
+++ Whambulance/Assets/Code/HUD/Elements/InGameTimer.cs
@@ -1,23 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InGameTimer : MonoBehaviour
+public class InGameTimer : HUDElement
 {
+    GameSettings settings;
+    InGameTimer inGameTimer;
 
-    float deliveryTime;
-    float rescueTime;
+    float maxDeliveryTime;
+    float maxRescueTime;
     float totalTime;
+
+    public Gradient timerGradient;
+    public Image circleTimerImage;
+
+    [Range(0, 1)]
+    public float radialDecimal;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        settings = GameManager.Settings;
+
+        maxDeliveryTime = settings.maxDeliveryTime;
+        maxRescueTime = settings.maxRescueTime;
+        totalTime = maxRescueTime + maxDeliveryTime;
+    }
+
+    public override bool ShouldDisplay
+    {
+        get
+        {
+            return GameManager.IsPlaying;
+        }
+    }
+
+    private void OnEnable()
+    {
+        inGameTimer = this;
+        GameManager.onWon += OnWon;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.onWon -= OnWon;
+    }
+
+    public void OnWon()
+    {
+        this.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateTimeLeft();
+        UpdateTimerColor();
+    }
+
+    private void CalculateTimeLeft()
+    {
+        float timePassed = maxRescueTime - GameManager.RescuingTime;
+        radialDecimal = timePassed / maxRescueTime;
+        circleTimerImage.fillAmount = radialDecimal;
+    }
+
+    private void UpdateTimerColor()
+    {
+        Color timerColor = timerGradient.Evaluate(radialDecimal);
+        GetComponentInChildren<Image>().color = timerColor;
     }
 }
