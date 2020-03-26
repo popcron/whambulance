6bb55209189e4689c34e0beaaf19d04890380260
Phill <philldamaskin@gmail.com>
2020-03-26 13:03:16 -0400

    Add generic health component

---
+++ Whambulance/Assets/Code/Components/Health.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public delegate void OnDied(Health health);
+    public delegate void OnDamaged(Health health, int damage);
+    public delegate void OnHealed(Health health, int heal);
+
+    public static OnDied onDied;
+    public static OnDamaged onDamaged;
+    public static OnHealed onHealed;
+
+    [SerializeField]
+    private int health = 12;
+
+    [SerializeField]
+    private int maxHealth = 12;
+
+    /// <summary>
+    /// Is this health component considered dead?
+    /// </summary>
+    public bool IsDead => health <= 0;
+
+    /// <summary>
+    /// The current amount of health.
+    /// </summary>
+    public int HP => health;
+
+    /// <summary>
+    /// The maximum health.
+    /// </summary>
+    public int MaxHP => maxHealth;
+
+    /// <summary>
+    /// Silently sets the health and max.
+    /// </summary>
+    public void Set(int health, int maxHealth)
+    {
+        this.health = health;
+        this.maxHealth = maxHealth;
+    }
+
+    /// <summary>
+    /// Damages this component.
+    /// </summary>
+    public void Damage(int amount)
+    {
+        if (amount > 0 && !IsDead)
+        {
+            int oldHealth = health;
+            health = Mathf.Clamp(health - amount, 0, maxHealth);
+            if (oldHealth > health)
+            {
+                onDamaged?.Invoke(this, amount);
+            }
+
+            if (health == 0)
+            {
+                onDied?.Invoke(this);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Heals this component.
+    /// </summary>
+    public void Heal(int amount)
+    {
+        if (amount > 0 && !IsDead)
+        {
+            int oldHealth = health;
+            health = Mathf.Clamp(health + amount, 0, maxHealth);
+            if (oldHealth < health)
+            {
+                onHealed?.Invoke(this, amount);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Instantly kills this health component.
+    /// </summary>
+    public void Kill()
+    {
+        if (!IsDead)
+        {
+            health = 0;
+            onDied?.Invoke(this);
+        }
+    }
+}
