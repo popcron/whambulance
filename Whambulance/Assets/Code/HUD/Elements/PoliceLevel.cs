811fa069a3299e3df3410011795d2fae61f60ac0
Phill <philldamaskin@gmail.com>
2020-03-30 11:55:34 -0400

    Fix sirens not positioned nicely

--- Whambulance/Assets/Code/HUD/Elements/PoliceLevel.cs
+++ Whambulance/Assets/Code/HUD/Elements/PoliceLevel.cs
@@ -58,7 +58,7 @@ public class PoliceLevel : HUDElement
         for (int i = 0; i < maxLevels; i++)
         {
             float t = i / (maxLevels - 1f);
-            RectTransform newSiren = Instantiate(prefab, prefab.transform);
+            RectTransform newSiren = Instantiate(prefab, prefab.parent);
             newSiren.anchoredPosition = Vector2.Lerp(min, max, t);
             sirens[i] = newSiren.Find("Siren").GetComponent<Image>();
         }
@@ -70,18 +70,27 @@ public class PoliceLevel : HUDElement
     {
         float difficulty = EnemyManager.Difficulty;
         int policeLevel = Mathf.FloorToInt(difficulty * maxLevels);
+        Vector2 min = Vector2.left * ((maxLevels * size * 0.5f) + size * 0.5f);
+        Vector2 max = Vector2.right * ((maxLevels * size * 0.5f) - size * 0.5f);
         for (int i = 0; i < sirens.Length; i++)
         {
+            //calculate color
+            float t = i / (maxLevels - 1f);
             bool isFlashing = i < policeLevel;
             if (isFlashing)
             {
-                float t = Mathf.PingPong(Time.time * flashingSpeed, 1f);
-                sirens[i].color = flashingColor.Evaluate(t);
+                float time = Time.time + Mathf.Abs(i.GetHashCode() * 0.8f);
+                float color = (time * flashingSpeed) % 1f;
+                sirens[i].color = flashingColor.Evaluate(color);
             }
             else
             {
                 sirens[i].color = idleColor;
             }
+
+            //position
+            RectTransform parent = sirens[i].transform.parent.GetComponent<RectTransform>();
+            parent.anchoredPosition = Vector2.Lerp(min, max, t);
         }
     }
 }
