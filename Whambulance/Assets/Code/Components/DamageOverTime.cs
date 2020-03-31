cf40c5772e918d9c2cf5fdd98e49aa101b000972
Phill <philldamaskin@gmail.com>
2020-03-30 20:59:09 -0400

    Add water pool areas

---
+++ Whambulance/Assets/Code/Components/DamageOverTime.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOverTime : MonoBehaviour
+{
+    [SerializeField]
+    private float damageInterval = 1f;
+
+    [SerializeField]
+    private string damageCause = "other";
+
+    private float damageTime;
+    private List<Health> healthsInside = new List<Health>();
+
+    private void FixedUpdate()
+    {
+        damageTime += Time.fixedDeltaTime;
+        bool damage = false;
+        if (damageTime > damageInterval)
+        {
+            damageTime = 0f;
+            damage = true;
+        }
+
+        if (damage)
+        {
+            for (int i = 0; i < healthsInside.Count; i++)
+            {
+                healthsInside[i].Damage(1, damageCause);
+            }
+        }
+
+        healthsInside.Clear();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Health health = collision.GetComponentInParent<Health>();
+        if (health)
+        {
+            healthsInside.Add(health);
+        }
+    }
+}
